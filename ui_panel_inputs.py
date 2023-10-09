64c9ad16138e438dfb84f3b6d544548d227ec710
Filip Dráber <draberf@gmail.com>
2023-10-09 13:15:09 +0200

    paste a dummy extension for node graph inspection

---
+++ ui_panel_inputs.py
@@ -0,0 +1,39 @@
+import bpy
+
+class UIInspectOperator(bpy.types.Operator):
+    bl_idname = "ui.inspector"
+    bl_label = "Inspector"
+
+    def execute(self, context):
+        nodes = bpy.context.material.node_tree.nodes
+        node = nodes[2]
+        print("name", node.name)
+        print("bl_label", node.bl_label)
+        print("bl_idname", node.bl_idname)
+        print("label", node.label)
+        return {'FINISHED'}
+        nodes = bpy.context.materials["Material"].nodes
+        print(nodes)
+        return {'FINISHED'}
+
+class UIInspectPanel(bpy.types.Panel):
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = "Inspect"
+    bl_idname = "NODE_EDITOR_PT_inspector"
+    bl_label = "Inspector"
+
+    @classmethod
+    def poll(cls, context):
+        return (context.object is not None)
+
+    def draw(self, context):
+        self.layout.label(text="Inspect")
+        self.layout.operator(
+            operator='ui.inspector',
+            icon='NODETREE',
+            text='Inspect'
+        )
+
+bpy.utils.register_class(UIInspectOperator)
+bpy.utils.register_class(UIInspectPanel)
