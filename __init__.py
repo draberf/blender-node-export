767ee20ec26e56f118d627b0916a9bc4799e625c
Filip Dráber <draberf@gmail.com>
2023-10-31 13:20:02 +0100

    write baseline init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,27 @@
+# source: https://wiki.blender.org/wiki/Process/Addons/Guidelines/metainfo
+
+
 bl_info = {
-    
+    "name": "Node Exporter to SVG",
+    "description": "Adds operator to export a node tree into an SVG image",
+    "author": "Filip Dráber",
+    "version": (0, 0, 1),
+    "blender": (3, 4, 0),
+    "location": "Node Editor",
+    "warning": "",
+    "support": "TESTING",
+    "doc_url": "",
+    "tracker_url": "",
+    "category": "Import-Export"
 }
+
+import bpy
+import src.ui_panel_inputs
+
+def register():
+    bpy.utils.register_class(src.ui_panel_inputs.UIInspectOperator)
+    bpy.utils.register_class(src.ui_panel_inputs.UIInspectPanel)
+
+def unregister():
+    pass
+
