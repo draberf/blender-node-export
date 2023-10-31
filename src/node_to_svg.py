ef0d62bd2fabaee2c2fe57cd44a05b7e68fd5400
Filip Dráber <draberf@gmail.com>
2023-10-31 21:58:25 +0100

    add preprocessing to offset all nodes

--- src/node_to_svg.py
+++ src/node_to_svg.py
@@ -8,6 +8,19 @@ def nodesToSvg(nodes: types.Node):
     doctype = "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">"
 
     svg = ET.Element('svg', width="20cm", height="8cm", version="1.1", xmlns="http://www.w3.org/2000/svg")
+    
+    # preprocess pass
+    # can be swapped with callbacks or something
+
+    min_x, min_y = 0, 0
+    for i, node in enumerate(nodes):
+        x, y = node.location
+        min_x = min(min_x, x)
+        min_y = min(min_y, y)
+
+
+    # generation pass
+
     for i, node in enumerate(nodes):
         g = ET.Element('g', id=f"{node.bl_name}_{i}")
         text = ET.Element('text')
