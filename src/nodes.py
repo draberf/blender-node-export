f9af142e5de6a9b7a59acaecacd73d9cddaeb1cc
Filip Dráber <draberf@gmail.com>
2023-12-05 00:24:42 +0100

    start defining node SVG

--- src/nodes.py
+++ src/nodes.py
@@ -10,11 +10,18 @@ class UINode(UI):
     
     node : bpy.types.Node
 
+    def svg(self):
+        group = ET.Element('g')
+        ...
+
 class UISocket(UI):
     
     socket : bpy.types.NodeSocket
 
     def svg(self):
         group = ET.Element('g')
-        label = ...
-
+        # p272
+        label = ET.Element('text')
+        label.text = self.socket.name
+        group.append(label)
+        return group
