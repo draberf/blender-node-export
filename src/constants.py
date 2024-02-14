8fe630238e2bf32ffd3bb7cb395924482fbfab5f
Filip Dráber <draberf@gmail.com>
2024-02-14 11:14:49 +0100

    move constants to file

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+# ratio of actual node frame dimensions to the bpy object
+NODE_DIM_RATI0 = 0.8
+
+# specific heights
+HEADER_HEIGHT = 20
+TOP_PADDING = 6.5
+SOCKET_GAP = 5.6
+BOTTOM_PADDING = 13.5
+
+LINKED_SOCKET_HEIGHT = 16
