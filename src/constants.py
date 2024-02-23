653dc210ac9560d3238fd730b1e84fee0519a0df
Filip Dráber <draberf@gmail.com>
2024-02-23 12:17:08 +0100

    add socket colors

--- src/constants.py
+++ src/constants.py
@@ -13,3 +13,25 @@ SOCKET_TEXT_HEIGHT = 11
 
 # colors
 HEADER_COLOR = "red"
+
+# properties
+DEFAULT_PROPERTIES = 32
+
+# colors
+SOCKET_COLORS = {
+    "CUSTOM": "#333333",
+    "VALUE": "#a1a1a1",
+    "INT": "#598c5c",
+    "BOOLEAN": "#cca6d6",
+    "VECTOR": "#6363c7",
+    "ROTATION": "#ff00ff",
+    "STRING": "#70b2ff",
+    "RGBA": "#c7c729",
+    "SHADER": "#633763",
+    "OBJECT": "#ed9e5c",
+    "IMAGE": "#633863",
+    "GEOMETRY": "#00d6a3",
+    "COLLECTION": "#f5f5f5",
+    "TEXTURE": "#9e4fa3",
+    "MATERIAL": "#eb7582"
+}
