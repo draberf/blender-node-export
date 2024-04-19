a8ab0843dd7db1431eeee7f6e80ac82e7a081470
Filip Dráber <draberf@gmail.com>
2024-04-19 15:06:49 +0200

    update version number

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ bl_info = {
     "name": "Node Exporter to SVG",
     "description": "Adds operator to export a node tree into an SVG image",
     "author": "Filip Dráber",
-    "version": (0, 2, 0),
+    "version": (0, 2, 1),
     "blender": (3, 4, 0),
     "location": "Node Editor",
     "warning": "",
