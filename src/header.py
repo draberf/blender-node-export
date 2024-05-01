492fc4ad6a00278791b2275b8f950d70ad5aa6e0
Filip Dráber <draberf@gmail.com>
2024-05-01 11:12:38 +0200

    update header visual

--- src/header.py
+++ src/header.py
@@ -5,7 +5,7 @@ from . import constants
 # class of SVG for a node header
 class UIHeader():
 
-    PADDING = 6
+    PADDING = 18
 
     def __init__(self, name, width=100, height=constants.HEADER_HEIGHT, color="gray"):
         self.name = name
@@ -15,13 +15,16 @@ class UIHeader():
 
     def svg(self, opacity=60) -> ET.Element:
         group = ET.Element('g', id=f"Header {self.name}")
-        rect = ET.SubElement(group, 'rect', attrib={
+        
+        ET.SubElement(group, 'rect', attrib={
             'width':f'{self.width}',
             'height':f'{self.height}',
             'opacity':str(opacity/100),
             'fill':self.color,
             'stroke':'none'
         })
+
+        ET.SubElement(group, 'use', href='#down_arrow', transform='translate(5,5)')
         
         label = ET.SubElement(group, 'text', x=f"{self.PADDING}", y=f"{self.height*3/4}")
         label.text = self.name
