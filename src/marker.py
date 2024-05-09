4b9e1145db376b6559ce094ca2a6ae860f805c66
Filip Dráber <draberf@gmail.com>
2024-05-09 14:31:13 +0200

    change whence the marker color comes from

--- src/marker.py
+++ src/marker.py
@@ -1,8 +1,5 @@
 import xml.etree.ElementTree as ET
 
-from . import constants
-
-
 class UIShape():
 
     shapes = {
@@ -14,14 +11,20 @@ class UIShape():
     def __init__(self, socket, render=True):
         self.shape = self.shapes[socket.display_shape[0]]
         self.has_dot = socket.display_shape[-1] == "T"
-        self.color = constants.SOCKET_COLORS[socket.type]
+        self.type = socket.type.lower()
         self.render = render
 
     def svg(self, x=0, y=0):
         if not self.render: return None
-        group = ET.Element('g', transform=f'translate({x},{y})')
+        group = ET.Element('g', attrib={
+            'transform':f'translate({x},{y})',
+            'class':'marker'
+        })
 
-        ET.SubElement(group, 'use', href=f'#marker_{self.shape}', fill=self.color)
-        if self.has_dot: ET.SubElement(group, 'use', href='#marker_dot', fill='black', stroke='none')
+        ET.SubElement(group, 'use', attrib={
+            'href':f'#marker_{self.shape}',
+            'class':self.type
+        })
+        if self.has_dot: ET.SubElement(group, 'use', href='#marker_dot', stroke='none')
 
         return group
